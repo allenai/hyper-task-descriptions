1b95fc64b6e6a389f2fcb700dab97adeb6afe9d7
Hamish Ivison <hamishivi@gmail.com>
2022-04-27 09:37:26 -0700

    replace template names with project names

--- setup.py
+++ setup.py
@@ -27,11 +27,11 @@ def read_requirements(filename: str):
 # version.py defines the VERSION and VERSION_SHORT variables.
 # We use exec here so we don't import cached_path whilst setting up.
 VERSION = {}  # type: ignore
-with open("my_project/version.py", "r") as version_file:
+with open("hyper_task_descriptions/version.py", "r") as version_file:
     exec(version_file.read(), VERSION)
 
 setup(
-    name="my_project",
+    name="hyper_task_descriptions",
     version=VERSION["VERSION"],
     description="",
     long_description=open("README.md").read(),
@@ -44,7 +44,7 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     keywords="",
-    url="https://github.com/allenai/my_project",
+    url="https://github.com/allenai/hyper_task_descriptions",
     author="Allen Institute for Artificial Intelligence",
     author_email="contact@allenai.org",
     license="Apache",
