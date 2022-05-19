cc0318451dc1aa0388607c16ec62a975fb3544ba
Hamish Ivison <hamishivi@gmail.com>
2022-05-19 15:50:53 -0700

    minor improvements

---
+++ hyper_task_descriptions/seqio_tasks/check_t0_tasks.py
@@ -0,0 +1,17 @@
+"""
+Quick script for checking if T0 caching is complete.
+"""
+from google.cloud import storage
+
+storage_client = storage.Client(project="ai2-allennlp")
+bucket = storage_client.bucket("hamishi-dev")
+
+name = "COMPLETED"
+
+tasks = [t.strip().decode("utf-8") for t in open("all_t0_tasks.txt", "rb").readlines()]
+
+for task in tasks:
+    if not storage.Blob(bucket=bucket, name=f"t0_data_roberta/{task}/COMPLETED").exists(
+        storage_client
+    ):
+        print(task)
