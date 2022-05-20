2e24a674bc2892a7ad24ff347cf99990212e1c71
Hamish Ivison <hamishivi@gmail.com>
2022-05-19 21:46:14 -0700

    fix

--- scripts/tpu_setup.sh
+++ scripts/tpu_setup.sh
@@ -10,10 +10,10 @@ python3 -m pip install git+https://github.com/hamishivi/seqio.git
 echo "----- ALL DEPENDENCIES INSTALLED -----"
 # next, we cache the tokenizers / HF splits used so we don't have to load them later.
 # This can take ~15 minutes.
-python3 -c "from transfomers import AutoTokenizer; AutoTokenizer.from_pretrained('t5-base')"
-python3 -c "from transfomers import AutoTokenizer; AutoTokenizer.from_pretrained('roberta-base')"
+python3 -c "from transformers import AutoTokenizer; AutoTokenizer.from_pretrained('t5-base')"
+python3 -c "from transformers import AutoTokenizer; AutoTokenizer.from_pretrained('roberta-base')"
 python3 -c "from transformers import FlaxRobertaModel; FlaxRobertaModel.from_pretrained('hamishivi/fixed-roberta-base')"
-TRANSFORMERS_OFFLINE=1 python3 -c "import hyper_task_descriptions.seqio_tasks.all_t0_tasks"
+TRANSFORMERS_OFFLINE=1 python3 -c "cd hyper-task-descriptions; import hyper_task_descriptions.seqio_tasks.all_t0_tasks"
 echo "----- CACHED TOKENIZERS AND SPLITS -----"
 # and we are done!
 echo "----- TPU SETUP COMPLETE -----"
