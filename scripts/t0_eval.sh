f8c2c0d96acb0f57331f275b93aacb92fa47450a
Hamish Ivison <hamishivi@gmail.com>
2022-05-24 12:14:15 -0700

    use cached tasks

--- scripts/t0_eval.sh
+++ scripts/t0_eval.sh
@@ -13,6 +13,7 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_search_paths="gins" \
     --gin_file="hyper_xl.gin" \
     --gin_file="t0_eval.gin" \
+    --gin.USE_CACHED_TASKS=True \
     --gin.utils.DatasetConfig.batch_size=128 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
