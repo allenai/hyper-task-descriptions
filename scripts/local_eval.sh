b71e68e8f90b067ab5598e67d02d231c63e10977
Hamish Ivison <hamishivi@gmail.com>
2022-06-01 16:38:07 -0700

    add new configs for catwalk eval

---
+++ scripts/local_eval.sh
@@ -0,0 +1,10 @@
+# li'l script for testing eval stuff locally.
+
+python3 -m t5x.eval \
+    --gin_search_paths="gins" \
+    --gin_file="hyper_xl.gin" \
+    --gin_file="catwalk_eval.gin" \
+    --gin.MIXTURE_OR_TASK_NAME=\"eleuther::cola\" \
+    --gin.EVAL_OUTPUT_DIR=\"model_eval_test\" \
+    --gin.CHECKPOINT_PATH=\"gs://hamishi-tpu-bucket/roberta_contrastive_dup_t0_3b/model/checkpoint_1123000\" \
+    --gin.USE_CACHED_TASKS=False
