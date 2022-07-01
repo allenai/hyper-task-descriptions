c52165579ac4f1df47d3bccef40417d85bf6014f
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-07-01 12:16:17 -0700

    tests for hyper network

--- tests/modeling/hyper_network_test.py
+++ tests/modeling/hyper_network_test.py
@@ -60,7 +60,6 @@ class NetworkTest(parameterized.TestCase):
         }
 
     def test_t5_1_1_regression(self):
-        # TODO: reduce sizes and confirm math.
         np.random.seed(0)
         batch_size, max_decode_len, input_len, hyper_input_len = 2, 3, 4, 5
         batch = {
@@ -72,12 +71,20 @@ class NetworkTest(parameterized.TestCase):
             "decoder_target_tokens": np.random.randint(3, 10, size=(batch_size, max_decode_len)),
         }
         model = get_test_model(
-            emb_dim=13, head_dim=64, num_heads=8, mlp_dim=2048, vocab_size=10, num_encoder_layers=3
+            emb_dim=13,
+            head_dim=16,
+            num_heads=8,
+            mlp_dim=32,
+            vocab_size=10,
+            num_encoder_layers=1,
+            num_decoder_layers=1,
         )
         params = model.get_initial_variables(jax.random.PRNGKey(42), self.input_shapes)["params"]
         loss, _ = jax.jit(model.loss_fn)(params, batch, jax.random.PRNGKey(1))
-        self.assertAlmostEqual(loss, 19.44814, delta=0.05)  # TODO: confirm math
+        self.assertAlmostEqual(loss, 15.268721, delta=0.05)
 
-        # predicted, scores = model.predict_batch_with_aux(params, batch)
-        # np.testing.assert_array_equal(predicted, [[2, 1, 0], [0, 0, 0]])
-        # np.testing.assert_allclose(scores["scores"], [-3.0401115, -1.9265753], rtol=1e-3)
+        predicted, scores = model.predict_batch_with_aux(params, batch)
+        # predicted.shape = 2 x 3 (batch_size x max_decode_len) (best option)
+        np.testing.assert_array_equal(predicted, [[2, 6, 1], [2, 6, 5]])
+        # scores.shape = 2 (batch_size) (best option)
+        np.testing.assert_allclose(scores["scores"], [-3.5013323, -2.8256376], rtol=1e-3)
