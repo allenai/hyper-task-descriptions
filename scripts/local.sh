2a17e39fca94ad177553312ac11252b7af605d70
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-07-27 13:09:14 -0700

    update scripts

--- scripts/local.sh
+++ scripts/local.sh
@@ -5,5 +5,5 @@ TRANSFORMERS_OFFLINE=1 HF_DATASETS_OFFLINE=1 python -m t5x.train \
     --gin_file="t0_train_local.gin" \
     --gin.MODEL_DIR=\"test\" \
     --gin.USE_CACHED_TASKS=False \
-    --gin.TRAIN_STEPS=100 \
+    --gin.TRAIN_STEPS=1100100 \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_small/checkpoint_1100000\"
