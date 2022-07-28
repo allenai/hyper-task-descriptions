8e2aea88428018f44f75d598d960a49472ffdf17
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-07-28 01:21:51 +0000

    joined_kv

--- hyper_task_descriptions/modeling/lora_partitioning.py
+++ hyper_task_descriptions/modeling/lora_partitioning.py
@@ -6,21 +6,11 @@ for details on how this works.
 
 lora_axes_names_override = [
     (
-        r"encoder/layers_\d+/(self_attention|attention|encoder_decoder_attention)/(query|key|value)/lora_a",
-        ("embed", "kv"),
+        r"(encoder|decoder)/layers_\d+/(self_attention|attention|encoder_decoder_attention)/(query|key|value)/lora_a",
+        ("embed", "joined_kv"),
     ),
-    (r"encoder/layers_\d+/(self_attention|attention|encoder_decoder_attention)/(query|key|value)/lora_b",
-     ("embed", "kv")),
-    (r"encoder/layers_\d+/(self_attention|attention|encoder_decoder_attention)/out/lora_a", ("kv", "embed")),
-    (r"encoder/layers_\d+/(self_attention|attention|encoder_decoder_attention)/out/lora_b", ("kv", "embed")),
-
-
-    (
-        r"decoder/layers_\d+/(self_attention|attention|encoder_decoder_attention)/(query|key|value)/lora_a",
-        ("embed", "kv"),
-    ),
-    (r"decoder/layers_\d+/(self_attention|attention|encoder_decoder_attention)/(query|key|value)/lora_b",
-     ("embed", "kv")),
-    (r"decoder/layers_\d+/(self_attention|attention|encoder_decoder_attention)/out/lora_a", ("kv", "embed")),
-    (r"decoder/layers_\d+/(self_attention|attention|encoder_decoder_attention)/out/lora_b", ("kv", "embed")),
+    (r"(encoder|decoder)/layers_\d+/(self_attention|attention|encoder_decoder_attention)/(query|key|value)/lora_b",
+     ("embed", "joined_kv")),
+    (r"(encoder|decoder)/layers_\d+/(self_attention|attention|encoder_decoder_attention)/out/lora_a", ("joined_kv", "embed")),
+    (r"(encoder|decoder)/layers_\d+/(self_attention|attention|encoder_decoder_attention)/out/lora_b", ("joined_kv", "embed")),
 ]
