b1775479de8be18e62bf4dfde8bb87766adb81e9
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-07-29 11:57:03 -0700

    gins

--- hyper_task_descriptions/modeling/hyper_network.py
+++ hyper_task_descriptions/modeling/hyper_network.py
@@ -56,7 +56,6 @@ class HyperT5Config(T5Config):
     roberta_max_position_embeddings: int = 520
     roberta_type_vocab_size: int = 8
     roberta_vocab_size: int = 50272
-    add_lora: bool = False  # TODO: not being used right now.
     lora_hyper_gen: bool = False
     lora_rank: int = 2
 
