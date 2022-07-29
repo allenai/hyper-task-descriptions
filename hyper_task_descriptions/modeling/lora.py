f7369e4a3f87a73c083e9324a17089e2f0876f37
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-07-28 18:57:31 -0700

    fix hyper gen lora

--- hyper_task_descriptions/modeling/lora.py
+++ hyper_task_descriptions/modeling/lora.py
@@ -58,6 +58,43 @@ def lora_linear(
     return output
 
 
+def batch_lora_linear(
+    inputs: NumArray,
+    kernel: NumArray,
+    lora_a: NumArray,
+    lora_b: NumArray,
+    alpha: int,
+    rank: int,
+    axis: Union[Iterable[int], int] = -1,
+) -> NumArray:
+
+    axis = _canonicalize_tuple(axis)
+    k_axis = _normalize_axes(axis, inputs.ndim)
+    contract_ind = tuple(range(0, len(k_axis)))
+    dimension_numbers = ((k_axis, contract_ind), ((), ()))
+
+    # Linear computation: output = W0x
+    output = lax.dot_general(inputs, kernel, dimension_numbers)
+    # output = output + bias
+
+    # Lora addition: output += BAx
+    a_axis = _normalize_axes(axis, lora_a.ndim)
+    a_contract_ind = tuple(range(1, 1 + len(a_axis))) #_normalize_axes((-2,), lora_a.ndim)
+    a_dimension_numbers = ((a_axis, a_contract_ind), ((0,), (0,)))
+
+    x = lax.dot_general(inputs, lora_a, dimension_numbers=a_dimension_numbers)
+
+    b_axis = _normalize_axes((-1,), x.ndim)
+    # b_contract_ind = tuple(range(0, len(b_axis)))
+    b_contract_ind = tuple(range(1, 1 + len(b_axis))) #_normalize_axes((-3,), lora_b.ndim) #tuple(range(lora_b.ndim-1, len(b_axis)))
+    b_dimension_numbers = ((b_axis, b_contract_ind), ((0,), (0,)))
+    x = lax.dot_general(x, lora_b, dimension_numbers=b_dimension_numbers)
+
+    output = output + x * (alpha / rank)
+
+    return output
+
+
 # Mostly copied from t5x/examples/t5/layers.py
 # ------------------------------------------------------------------------------
 # DenseGeneral for attention layers.
@@ -125,17 +162,25 @@ class LoraDenseGeneral(nn.Module):
             lora_b = jnp.asarray(lora_b, self.dtype)
             lora_b = jnp.reshape(lora_b, lora_b_shape)
 
-        # contract_ind = tuple(range(0, len(axis)))
-        # lax.dot_general(inputs, kernel, ((axis, contract_ind), ((), ())))
-        return lora_linear(
-            inputs,
-            kernel,
-            lora_a=lora_a,
-            lora_b=lora_b,
-            alpha=self.alpha,
-            rank=self.rank,
-            axis=self.axis,
-        )
+            return lora_linear(
+                inputs,
+                kernel,
+                lora_a=lora_a,
+                lora_b=lora_b,
+                alpha=self.alpha,
+                rank=self.rank,
+                axis=self.axis,
+            )
+        else:
+            return batch_lora_linear(
+                inputs,
+                kernel,
+                lora_a=lora_a,
+                lora_b=lora_b,
+                alpha=self.alpha,
+                rank=self.rank,
+                axis=self.axis,
+            )
 
 
 class LoraMultiHeadDotProductAttention(nn.Module):
@@ -360,6 +405,7 @@ class LoraMultiHeadDotProductAttention(nn.Module):
             kernel_axes=("joined_kv", "embed"),
             dtype=self.dtype,
             name="out",
+            hyper_gen=self.hyper_gen,
         )(x, lora_a=lora_oa, lora_b=lora_ob)
         return out
 
--- tests/modeling/lora_network_test.py
+++ tests/modeling/lora_network_test.py
@@ -24,7 +24,7 @@ class NetworkTest(parameterized.TestCase):
             "decoder_target_tokens": np.random.randint(3, 10, size=(batch_size, max_decode_len)),
         }
 
-    def test_t5_1_1_regression(self):
+    def test_t5_1_1_lora(self):
         np.random.seed(0)
         batch_size, max_decode_len, input_len, hyper_input_len = 2, 3, 4, 5
         batch = {
@@ -44,6 +44,7 @@ class NetworkTest(parameterized.TestCase):
             num_encoder_layers=1,
             num_decoder_layers=1,
             do_lora=True,
+            lora_hyper_gen=False,
         )
         params = model.get_initial_variables(jax.random.PRNGKey(42), self.input_shapes)["params"]
         loss, _ = jax.jit(model.loss_fn)(params, batch, jax.random.PRNGKey(1))
@@ -54,3 +55,41 @@ class NetworkTest(parameterized.TestCase):
         np.testing.assert_array_equal(predicted, [[2, 6, 1], [2, 6, 5]])
         # scores.shape = 2 (batch_size) (best option)
         np.testing.assert_allclose(scores["scores"], [-3.5013323, -2.8256376], rtol=1e-3)
+
+    def test_t5_1_1_hyper_lora(self):
+        np.random.seed(0)
+        batch_size, max_decode_len, input_len, hyper_input_len = 2, 3, 4, 5
+        batch = {
+            "encoder_input_tokens": np.random.randint(3, 10, size=(batch_size, input_len)),
+            "hyper_encoder_input_tokens": np.random.randint(
+                3, 10, size=(batch_size, hyper_input_len)
+            ),
+            "decoder_input_tokens": np.random.randint(3, 10, size=(batch_size, max_decode_len)),
+            "decoder_target_tokens": np.random.randint(3, 10, size=(batch_size, max_decode_len)),
+        }
+        model = get_test_model(
+            emb_dim=13,
+            head_dim=16,
+            num_heads=8,
+            mlp_dim=32,
+            vocab_size=10,
+            num_encoder_layers=1,
+            num_decoder_layers=1,
+            do_lora=True,
+            lora_hyper_gen=True,
+        )
+        params = model.get_initial_variables(jax.random.PRNGKey(42), self.input_shapes)["params"]
+        loss, _ = jax.jit(model.loss_fn)(params, batch, jax.random.PRNGKey(1))
+        self.assertAlmostEqual(loss, 15.268721, delta=0.05)
+
+        predicted, scores = model.predict_batch_with_aux(params, batch)
+        # predicted.shape = 2 x 3 (batch_size x max_decode_len) (best option)
+        np.testing.assert_array_equal(predicted, [[2, 6, 1], [2, 6, 5]])
+        # scores.shape = 2 (batch_size) (best option)
+        np.testing.assert_allclose(scores["scores"], [-3.5013323, -2.8256376], rtol=1e-3)
+
+
+if __name__ == "__main__":
+    nt = NetworkTest()
+    nt.setUp()
+    nt.test_t5_1_1_regression()
