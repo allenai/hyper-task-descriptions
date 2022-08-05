8de8c97fec51092f73a5ff70aa48657a506e8f64
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-08-05 07:28:35 +0000

    updated xl gin

--- scripts/train_lora_from_t5.sh
+++ scripts/train_lora_from_t5.sh
@@ -9,12 +9,12 @@ MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
 # for your first run, you will probably need to run all these calls :(
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.train \
   --gin_search_paths=gins \
-  --gin_file="lora/lora_small.gin" \
+  --gin_file="lora/lora_xl.gin" \
   --gin_file="t0_train.gin" \
   --gin_file="partial_train_adam.gin" \
   --gin_file="lora/lora.gin" \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=1212200 \
   --gin.partitioning.PjitPartitioner.num_partitions=8 \
-  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_small/checkpoint_1100000\" \
+  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000\" \
   --tfds_data_dir="gs://hamishi-us-bucket/t0_data/data"
