f7568eecbdb1ca5df314d923ff87a9ec34dec847
yizhongw <eastonwyz@gmail.com>
2022-08-06 15:59:07 -0700

    add regular training for ni.

--- scripts/ni_reg_eval.sh
+++ scripts/ni_reg_eval.sh
@@ -14,7 +14,7 @@ python3 -m t5x.eval \
     --gin_search_paths="gins" \
     --gin_file="hyper_small.gin" \
     --gin_file="ni_eval.gin" \
-    --gin.MIXTURE_OR_TASK_NAME="natural_instructions_def_pos_2" \
+    --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def_pos_2\" \
     --gin.USE_CACHED_TASKS=False \
     --gin_file=hyper_network.HyperT5Config.add_adapters=False \
     --gin.utils.DatasetConfig.batch_size=1024 \
--- scripts/ni_reg_train.sh
+++ scripts/ni_reg_train.sh
@@ -10,7 +10,7 @@ python3 -m t5x.train \
   --gin_search_paths=gins \
   --gin_file="hyper_xl.gin" \
   --gin_file="ni_train.gin" \
-  --gin.MIXTURE_OR_TASK_NAME="natural_instructions_def_pos_2" \
+  --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def_pos_2\" \
   --gin.USE_CACHED_TASKS=False \
   --gin_file=hyper_network.HyperT5Config.add_adapters=False \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
