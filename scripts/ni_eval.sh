99ee14deb1a524bc71b71e206c1815c5c33e73b1
yizhongw <eastonwyz@gmail.com>
2022-08-07 00:49:09 -0700

    update evaluation mode.

--- scripts/ni_eval.sh
+++ scripts/ni_eval.sh
@@ -15,8 +15,8 @@ python3 -m t5x.eval \
     --gin_file="hyper_xl.gin" \
     --gin_file="ni_eval.gin" \
     --gin.USE_CACHED_TASKS=False \
-    --gin.utils.DatasetConfig.batch_size=256 \
+    --gin.utils.DatasetConfig.batch_size=1024 \
     --gin.utils.DatasetConfig.split=\"test\" \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
-    --gin.utils.RestoreCheckpointConfig.mode=\"specific\" \
+    --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
