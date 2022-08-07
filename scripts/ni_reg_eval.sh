ceecb21adebaa251fd0e3ec717a7d60b432ad708
yizhongw <eastonwyz@gmail.com>
2022-08-06 22:12:19 -0700

    fix script for ni eval.

--- scripts/ni_reg_eval.sh
+++ scripts/ni_reg_eval.sh
@@ -12,12 +12,11 @@ EVAL_OUTPUT_DIR="gs://yizhongw-tpu-bucket/${EXPERIMENT_NAME}/eval/"
 # note you pass in a model file and the eval file.
 python3 -m t5x.eval \
     --gin_search_paths="gins" \
-    --gin_file="hyper_small.gin" \
+    --gin_file="hyper_xl.gin" \
     --gin_file="ni_eval.gin" \
     --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def_pos_2\" \
     --gin.USE_CACHED_TASKS=False \
-    --gin.hyper_network.HyperT5Config.add_adapters=False \
     --gin.utils.DatasetConfig.batch_size=1024 \
-    --gin.utils.DatasetConfig.split=\"train\" \
+    --gin.utils.DatasetConfig.split=\"test\" \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
