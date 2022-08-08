779cf5af62442fc4b8a903a96e7c95679b90160c
yizhongw <eastonwyz@gmail.com>
2022-08-07 19:36:11 -0700

    debugging.

--- scripts/t0_reg_eval.sh
+++ scripts/t0_reg_eval.sh
@@ -8,13 +8,13 @@ EVAL_OUTPUT_DIR="gs://hamishi-us-bucket/${SAVE_DIR}"
 # we go offline to avoid constant calls to get basic info (happens even when cached)
 # for your first run, you will probably need to run all these calls :(
 # note you pass in a model file and the eval file.
-python3 -m t5x.eval \
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_search_paths="gins" \
-    --gin_file="t5x/examples/t5/t5_1_1/xl.gin" \
+    --gin_file="hyper_xl.gin" \
     --gin_file="t0_eval.gin" \
+    --gin.hyper_network.HyperT5Config.add_adapters=False \
     --gin.USE_CACHED_TASKS=True \
     --gin.partitioning.PjitPartitioner.num_partitions=8 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
-    --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
