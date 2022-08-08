cc1f6253332cb86183c748a8c1ca8762c79434b0
yizhongw <eastonwyz@gmail.com>
2022-08-07 20:39:53 -0700

    debugging.

--- scripts/t0_reg_eval.sh
+++ scripts/t0_reg_eval.sh
@@ -10,9 +10,10 @@ EVAL_OUTPUT_DIR="gs://hamishi-us-bucket/${SAVE_DIR}"
 # note you pass in a model file and the eval file.
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_search_paths="gins" \
-    --gin_file="t5x/examples/t5/t5_1_1/xl.gin"
+    --gin_file="hyper_xl.gin" \
     --gin_file="t0_eval.gin" \
     --gin.USE_CACHED_TASKS=True \
     --gin.utils.DatasetConfig.batch_size=128 \
+    --gin.partitioning.PjitPartitioner.num_partitions=8 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
