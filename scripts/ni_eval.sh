86b71029fc8de2abc92e53c43ed8ff35b96d8015
yizhongw <eastonwyz@gmail.com>
2022-08-13 22:46:59 -0700

    change eval setting.

--- scripts/ni_eval.sh
+++ scripts/ni_eval.sh
@@ -15,9 +15,9 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_file="hyper_xl.gin" \
     --gin_file="ni_eval.gin" \
     --gin.USE_CACHED_TASKS=True \
-    --gin.utils.DatasetConfig.batch_size=1024 \
+    --gin.utils.DatasetConfig.batch_size=512 \
     --gin.utils.DatasetConfig.split=\"test\" \
-    --gin.partitioning.PjitPartitioner.num_partitions=16 \
+    --gin.partitioning.PjitPartitioner.num_partitions=8 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
