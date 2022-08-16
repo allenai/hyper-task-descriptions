4946c029d687877ec10b09f7590b32bc16e86a00
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-08-15 17:36:54 -0700

    add plain lora, refactor

--- tests/modeling/lora_network_test.py
+++ tests/modeling/lora_network_test.py
@@ -26,12 +26,12 @@ class NetworkTest(parameterized.TestCase):
 
     def test_t5_1_1_lora(self):
         np.random.seed(0)
-        batch_size, max_decode_len, input_len, hyper_input_len = 2, 3, 4, 5
+        batch_size, max_decode_len, input_len = 2, 3, 4
         batch = {
             "encoder_input_tokens": np.random.randint(3, 10, size=(batch_size, input_len)),
-            "hyper_encoder_input_tokens": np.random.randint(
-                3, 10, size=(batch_size, hyper_input_len)
-            ),
+            # "hyper_encoder_input_tokens": np.random.randint(
+            #     3, 10, size=(batch_size, hyper_input_len)
+            # ),
             "decoder_input_tokens": np.random.randint(3, 10, size=(batch_size, max_decode_len)),
             "decoder_target_tokens": np.random.randint(3, 10, size=(batch_size, max_decode_len)),
         }
@@ -49,14 +49,15 @@ class NetworkTest(parameterized.TestCase):
         )
         params = model.get_initial_variables(jax.random.PRNGKey(42), self.input_shapes)["params"]
 
-        assert "lora_qa_gen" not in params["hyper"]
+        # assert "lora_qa_gen" not in params["hyper"]
+        assert "hyper" not in params
         assert "lora_a" in params["encoder"]["layers_0"]["attention"]["query"]
         assert "lora_a" not in params["encoder"]["layers_0"]["attention"]["key"]
         assert "lora_a" in params["encoder"]["layers_0"]["attention"]["value"]
         assert "lora_a" not in params["encoder"]["layers_0"]["attention"]["out"]
 
         loss, _ = jax.jit(model.loss_fn)(params, batch, jax.random.PRNGKey(1))
-        self.assertAlmostEqual(loss, 17.351202, delta=0.05)
+        self.assertAlmostEqual(loss, 16.716328, delta=0.05)
 
         predicted, scores = model.predict_batch_with_aux(params, batch)
         # predicted.shape = 2 x 3 (batch_size x max_decode_len) (best option)
