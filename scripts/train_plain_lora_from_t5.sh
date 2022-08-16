9551b2a8d4fc2a2d91c7c57f8d90d4318787adad
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-08-16 07:51:10 +0000

    xl script

--- scripts/train_plain_lora_from_t5.sh
+++ scripts/train_plain_lora_from_t5.sh
@@ -2,19 +2,19 @@
 EXPERIMENT_NAME=$1
 
 # where model will be saved
-# MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
-MODEL_DIR="${EXPERIMENT_NAME}"
+MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
+# MODEL_DIR="${EXPERIMENT_NAME}"
 
 # we go offline to avoid constant calls to get basic info (happens even when cached)
 # for your first run, you will probably need to run all these calls :(
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.train \
   --gin_search_paths=gins \
-  --gin_file="lora/plain/lora_small.gin" \
-  --gin_file="t0_train_local.gin" \
+  --gin_file="lora/plain/lora_xl.gin" \
+  --gin_file="t0_train.gin" \
   --gin_file="partial_train_adam.gin" \
   --gin_file="lora/lora.gin" \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=1107000 \
-  --gin.partitioning.PjitPartitioner.num_partitions=1 \
-  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_small/checkpoint_1100000\" \
+  --gin.partitioning.PjitPartitioner.num_partitions=8 \
+  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000\" \
   --tfds_data_dir="gs://hamishi-us-bucket/t0_data/data"
