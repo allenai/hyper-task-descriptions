80e0c843510ba957ca111863e51757ee390461bc
yizhongw <eastonwyz@gmail.com>
2022-08-17 10:13:52 -0700

    increase bottleneck size

--- scripts/ni_train.sh
+++ scripts/ni_train.sh
@@ -14,10 +14,11 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.train \
   --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
   --gin.USE_CACHED_TASKS=True \
   --gin.utils.DatasetConfig.batch_size=1024 \
+  --gin.trainer.Trainer.num_microbatches=32 \
   --gin.utils.DatasetConfig.split=\"train\" \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=1106000 \
-  --gin.hyper_network.HyperT5Config.hbottleneck_size=160 \
+  --gin.hyper_network.HyperT5Config.hbottleneck_size=256 \
   --gin.partitioning.PjitPartitioner.num_partitions=8 \
   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000/\" \
   --tfds_data_dir=\"gs://yizhongw-tpu-bucket/t0_data/data\"
@@ -30,9 +31,9 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_file="ni_eval.gin" \
     --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
     --gin.USE_CACHED_TASKS=True \
-    --gin.utils.DatasetConfig.batch_size=1024 \
+    --gin.utils.DatasetConfig.batch_size=512 \
     --gin.utils.DatasetConfig.split=\"test\" \
-    --gin.hyper_network.HyperT5Config.hbottleneck_size=160 \
+    --gin.hyper_network.HyperT5Config.hbottleneck_size=256 \
     --gin.partitioning.PjitPartitioner.num_partitions=8 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
