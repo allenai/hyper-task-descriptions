97b6323f8b00804220f891b848f59c808cbec5a3
yizhongw <eastonwyz@gmail.com>
2022-09-14 20:17:36 -0700

    temporary

--- scripts/ni_train.sh
+++ scripts/ni_train.sh
@@ -9,24 +9,24 @@ MODEL_DIR="gs://yizhongw-tpu-bucket/${EXPERIMENT_NAME}/model"
 
 # we go offline to avoid constant calls to get basic info (happens even when cached)
 # for your first run, you will probably need to run all these calls :(
-HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.train \
-  --gin_search_paths=gins \
-  --gin_file="hyper_base.gin" \
-  --gin_file="ni_train.gin" \
-  --gin_file="partial_train_adam.gin" \
-  --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
-  --gin.USE_CACHED_TASKS=True \
-  --gin.trainer.Trainer.num_microbatches=32 \
-  --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
-  --gin.BATCH_SIZE=1024 \
-  --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
-  --gin.hyper_network.HyperT5Config.use_adapter=False \
-  --gin.hyper_network.HyperT5Config.use_prefix=False \
-  --gin.hyper_network.HyperT5Config.use_simple_prefix_vectors=True \
-  --gin.MODEL_DIR=\"${MODEL_DIR}\" \
-  --gin.TRAIN_STEPS=1101000 \
-  --gin.partitioning.PjitPartitioner.num_partitions=4 \
-  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\" \
+# HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.train \
+#   --gin_search_paths=gins \
+#   --gin_file="hyper_base.gin" \
+#   --gin_file="ni_train.gin" \
+#   --gin_file="partial_train_adam.gin" \
+#   --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
+#   --gin.USE_CACHED_TASKS=True \
+#   --gin.trainer.Trainer.num_microbatches=32 \
+#   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
+#   --gin.BATCH_SIZE=1024 \
+#   --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
+#   --gin.hyper_network.HyperT5Config.use_adapter=False \
+#   --gin.hyper_network.HyperT5Config.use_prefix=False \
+#   --gin.hyper_network.HyperT5Config.use_simple_prefix_vectors=True \
+#   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
+#   --gin.TRAIN_STEPS=1101000 \
+#   --gin.partitioning.PjitPartitioner.num_partitions=4 \
+#   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\" \
 
 echo "Training done. Now evaluating all checkpoints..."
 
