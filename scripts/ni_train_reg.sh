78e26e6c5db67f10457e248c3ca4b1e71f107dbc
Hamish Ivison <hamishivi@gmail.com>
2022-10-12 16:11:42 -0700

    stuff

--- scripts/ni_train_reg.sh
+++ scripts/ni_train_reg.sh
@@ -9,24 +9,24 @@ MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
 
 # we go offline to avoid constant calls to get basic info (happens even when cached)
 # for your first run, you will probably need to run all these calls :(
-python3 -m t5x.train \
-  --gin_search_paths=gins \
-  --gin_file="hyper_base.gin" \
-  --gin_file="ni_train.gin" \
-  --gin_file="partial_train_adam.gin" \
-  --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
-  --gin.hyper_network.HyperT5Config.use_adapter=False \
-  --gin.hyper_network.HyperT5Config.use_prefix=False \
-  --gin.hyper_network.HyperT5Config.use_instructions=False \
-  --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
-  --gin.USE_CACHED_TASKS=True \
-  --gin.trainer.Trainer.num_microbatches=8 \
-  --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
-  --gin.BATCH_SIZE=1024 \
-  --gin.MODEL_DIR=\"${MODEL_DIR}\" \
-  --gin.TRAIN_STEPS=1101000 \
-  --gin.partitioning.PjitPartitioner.num_partitions=2 \
-  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\" \
+# python3 -m t5x.train \
+#   --gin_search_paths=gins \
+#   --gin_file="hyper_base.gin" \
+#   --gin_file="ni_train.gin" \
+#   --gin_file="partial_train_adam.gin" \
+#   --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
+#   --gin.hyper_network.HyperT5Config.use_adapter=False \
+#   --gin.hyper_network.HyperT5Config.use_prefix=False \
+#   --gin.hyper_network.HyperT5Config.use_instructions=False \
+#   --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
+#   --gin.USE_CACHED_TASKS=True \
+#   --gin.trainer.Trainer.num_microbatches=8 \
+#   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
+#   --gin.BATCH_SIZE=1024 \
+#   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
+#   --gin.TRAIN_STEPS=1101000 \
+#   --gin.partitioning.PjitPartitioner.num_partitions=2 \
+#   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\" \
 
 echo "Training done. Now evaluating all checkpoints..."
 
@@ -34,7 +34,6 @@ EVAL_OUTPUT_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/eval/"
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_search_paths="gins" \
     --gin_file="hyper_base.gin" \
-    --gin_file="instruction_embed.gin" \
     --gin_file="ni_eval.gin" \
     --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
     --gin.hyper_network.HyperT5Config.use_adapter=False \
