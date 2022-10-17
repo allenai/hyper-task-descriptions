58f5ce633ea3067ef374173ac016a32fb5d819fa
Hamish Ivison <hamishivi@gmail.com>
2022-10-17 12:59:53 -0700

    pretrain with linear

--- scripts/ni_train.sh
+++ scripts/ni_train.sh
@@ -24,7 +24,7 @@ python3 -m t5x.train \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=1101000 \
   --gin.partitioning.PjitPartitioner.num_partitions=2 \
-  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\" \
+  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\"
 
 echo "Training done. Now evaluating all checkpoints..."
 
