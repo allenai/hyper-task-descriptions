a9ffaf73105e66473e035db33d5ce9fbbadc7a8c
Hamish Ivison <hamishivi@gmail.com>
2022-10-18 10:07:02 -0700

    fix: forgot a slash

--- scripts/t0_reg_eval.sh
+++ scripts/t0_reg_eval.sh
@@ -19,5 +19,5 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin.hyper_network.HyperT5Config.use_instructions=False \
     --gin.utils.DatasetConfig.batch_size=128 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
-    --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
+    --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\" \
     --gin.utils.RestoreCheckpointConfig.mode=\"all\"
