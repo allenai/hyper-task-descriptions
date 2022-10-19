40b0dd83aa84b56acadf36ffabe85683efead12c
Hamish Ivison <hamishivi@gmail.com>
2022-10-19 09:33:14 -0700

    new pretrained

--- hyper_task_descriptions/python_scripts/make_p3_boxplot.py
+++ hyper_task_descriptions/python_scripts/make_p3_boxplot.py
@@ -3,7 +3,7 @@ import sys
 import matplotlib.pyplot as plt
 
 with open(sys.argv[1], "r") as f:
-    data = [x.strip().split(",") for x in f.readlines()][1:]
+    data = [x.strip().split("\t") for x in f.readlines()][1:]
 
 tasks = sorted(list(set([x[0] for x in data])))
 
@@ -18,31 +18,15 @@ def get_model_data(model_idx, data):
     return task_values
 
 
-palm_0 = get_model_data(2, data)
-palm_1 = get_model_data(3, data)
+original_t0_3b = get_model_data(3, data)
+my_t0_3b = get_model_data(2, data)
+original_t0_11b = get_model_data(6, data)
 
-t0_3b_vals = get_model_data(4, data)
-t0_11b_vals = get_model_data(5, data)
-t0p_11b_vals = get_model_data(6, data)
-t0pp_11b_vals = get_model_data(7, data)
-
-t03b4k_vals = get_model_data(8, data)
-t03b16k_vals = get_model_data(9, data)
-
-my_vals = get_model_data(10, data)
-my_vals_14 = get_model_data(11, data)
-my_vals_26 = get_model_data(12, data)
-
-cont_5 = get_model_data(13, data)
-cont_9 = get_model_data(15, data)
-nu_cont = get_model_data(16, data)
-base = get_model_data(18, data)
-large = get_model_data(21, data)
 
 fig, axs = plt.subplots(2, 6)
 
-models_to_eval = [t0_3b_vals, t0_11b_vals, t03b16k_vals, base, large]
-model_names = ["T0 3B", "T0", "T03B+16K", "base16k", "6k"]
+models_to_eval = [original_t0_3b, my_t0_3b, original_t0_11b]
+model_names = ["T0 3B", "T03B+16K", "T011B"]
 print(tasks)
 for i, task in enumerate(tasks):
     vert_idx = i // 6
