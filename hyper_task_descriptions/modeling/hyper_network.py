a447c65f8fa832ebbd578758c67235f630ce22b8
Hamish Ivison <hamishivi@gmail.com>
2022-10-20 11:17:31 -0700

    Fix

--- hyper_task_descriptions/modeling/hyper_network.py
+++ hyper_task_descriptions/modeling/hyper_network.py
@@ -732,8 +732,8 @@ class HyperEncoder(nn.Module):
 
         # concat. currently not using mask cor. but thats ok
         if cfg.use_instruction_embedding:
-            #     adaptations.pop('hyper_encoder_input_tokens')
-            #     adaptations.pop('instruction_embedding')
+            adaptations.pop('hyper_encoder_input_tokens')
+            adaptations.pop('instruction_embedding')
             encoder_tokens = jnp.concatenate(
                 [adaptations.pop('hyper_encoder_input_tokens'), encoder_input_tokens],
                 axis=1)
