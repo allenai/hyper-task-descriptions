cef2930e500d42d9c9f06a22949f9f16b3b5d6fe
Hamish Ivison <hamishivi@gmail.com>
2022-10-21 12:01:35 -0700

    stuff

--- scripts/ni_train.sh
+++ scripts/ni_train.sh
@@ -22,7 +22,7 @@ python3 -m t5x.train \
   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
   --gin.BATCH_SIZE=1024 \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
-  --gin.TRAIN_STEPS=1101000 \
+  --gin.TRAIN_STEPS=1103000 \
   --gin.partitioning.PjitPartitioner.num_partitions=2 \
   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\"
 
