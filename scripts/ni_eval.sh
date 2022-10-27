fa127a9d3ac65e51ea254601836144eee023d808
Hamish Ivison <hamishivi@gmail.com>
2022-10-27 15:39:09 -0700

    hmmm

--- scripts/ni_eval.sh
+++ scripts/ni_eval.sh
@@ -5,7 +5,7 @@
 EXPERIMENT_NAME=$1
 
 # where model will be saved
-MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
+MODEL_DIR="${EXPERIMENT_NAME}/model"
 EVAL_OUTPUT_DIR="${EXPERIMENT_NAME}/eval/"
 
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
@@ -14,7 +14,7 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_file="instruction_embed.gin" \
     --gin_file="ni_eval.gin" \
     --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
-    --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_hyper_alt\" \
+    --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
     --gin.USE_CACHED_TASKS=True \
     --gin.utils.DatasetConfig.batch_size=512 \
     --gin.utils.DatasetConfig.split=\"test\" \
