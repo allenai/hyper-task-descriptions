97742b5054d51ba04782d0f4734d9a3f5ca34979
Hamish Ivison <hamishivi@gmail.com>
2022-10-29 12:01:52 -0700

    fix

--- hyper_task_descriptions/modeling/hyper_network.py
+++ hyper_task_descriptions/modeling/hyper_network.py
@@ -748,7 +748,7 @@ class HyperEncoder(nn.Module):
                 [adaptations.pop('hyper_encoder_input_tokens'), encoder_input_tokens],
                 axis=1)
             lyr_encoder_mask = layers.make_attention_mask(
-                encoder_input_tokens > 0, encoder_input_tokens > 0, dtype=cfg.dtype
+                encoder_tokens > 0, encoder_tokens > 0, dtype=cfg.dtype
             )
             instruction_embeds = adaptations.pop('instruction_embedding_layers')
 
