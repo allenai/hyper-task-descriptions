ee23358628994a22953c4ac1ef6ddb649347af47
Hamish Ivison <hamishivi@gmail.com>
2022-10-29 11:59:54 -0700

    stuff

--- hyper_task_descriptions/modeling/hyper_network.py
+++ hyper_task_descriptions/modeling/hyper_network.py
@@ -754,17 +754,17 @@ class HyperEncoder(nn.Module):
 
         for lyr in range(cfg.num_encoder_layers):
             layer_adaptations = {k: v[:, lyr] for k, v in adaptations.items()}
-            # if cfg.use_instruction_embedding and lyr == 0:
-            #     x = jnp.concatenate([instruction_embeds[lyr], x], axis=1)
+            if cfg.use_instruction_embedding and lyr == 0:
+                x = jnp.concatenate([embed, x], axis=1)
             # [batch, length, emb_dim] -> [batch, length, emb_dim]
             x = HyperEncoderLayer(config=cfg, relative_embedding=rel_emb, name=f"layers_{lyr}")(
                 x,
                 **layer_adaptations,
-                encoder_mask=encoder_mask,
+                encoder_mask=lyr_encoder_mask,
                 deterministic=deterministic,
             )
-            # if cfg.use_instruction_embedding:
-            #     x = x[:, embed.shape[1]:]
+            if cfg.use_instruction_embedding:
+                x = x[:, embed.shape[1]:]
 
         x = layers.LayerNorm(dtype=cfg.dtype, name="encoder_norm")(x)
         return nn.Dropout(rate=cfg.dropout_rate)(x, deterministic=deterministic)
