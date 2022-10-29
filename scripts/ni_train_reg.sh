71ee10b1b9f5658715ef5ea5d24b990f3b518baf
Hamish Ivison <hamishivi@gmail.com>
2022-10-28 17:50:05 -0700

    fixing up the eval scripts

--- scripts/ni_train_reg.sh
+++ scripts/ni_train_reg.sh
@@ -5,7 +5,7 @@
 EXPERIMENT_NAME=$1
 
 # where model will be saved
-MODEL_DIR="${EXPERIMENT_NAME}/model"
+MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
 
 # we go offline to avoid constant calls to get basic info (happens even when cached)
 # for your first run, you will probably need to run all these calls :(
@@ -29,23 +29,23 @@ python3 -m t5x.train \
   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\" \
 
 echo "Training done. Now evaluating all checkpoints..."
-gsutil -m cp -r ${MODEL_DIR} gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model
+# gsutil -m cp -r ${MODEL_DIR} gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model
 
 
-# EVAL_OUTPUT_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/eval/"
-# HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
-#     --gin_search_paths="gins" \
-#     --gin_file="hyper_base.gin" \
-#     --gin_file="ni_eval.gin" \
-#     --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
-#     --gin.hyper_network.HyperT5Config.use_adapter=False \
-#     --gin.hyper_network.HyperT5Config.use_prefix=False \
-#     --gin.hyper_network.HyperT5Config.use_instructions=False \
-#     --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
-#     --gin.USE_CACHED_TASKS=True \
-#     --gin.utils.DatasetConfig.batch_size=512 \
-#     --gin.utils.DatasetConfig.split=\"test\" \
-#     --gin.partitioning.PjitPartitioner.num_partitions=2 \
-#     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
-#     --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
-#     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
+EVAL_OUTPUT_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/eval/"
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
+    --gin_search_paths="gins" \
+    --gin_file="hyper_base.gin" \
+    --gin_file="ni_eval.gin" \
+    --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
+    --gin.hyper_network.HyperT5Config.use_adapter=False \
+    --gin.hyper_network.HyperT5Config.use_prefix=False \
+    --gin.hyper_network.HyperT5Config.use_instructions=False \
+    --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
+    --gin.USE_CACHED_TASKS=True \
+    --gin.utils.DatasetConfig.batch_size=512 \
+    --gin.utils.DatasetConfig.split=\"test\" \
+    --gin.partitioning.PjitPartitioner.num_partitions=2 \
+    --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
+    --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
+    --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
