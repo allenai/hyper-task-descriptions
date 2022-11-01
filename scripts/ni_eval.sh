2932de619f277ea8ca55a550fc90af8adc4230c2
Hamish Ivison <hamishivi@gmail.com>
2022-10-31 23:05:37 -0700

    eval

--- scripts/ni_eval.sh
+++ scripts/ni_eval.sh
@@ -6,11 +6,11 @@ EXPERIMENT_NAME=$1
 
 # where model will be saved
 MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
-EVAL_OUTPUT_DIR="${EXPERIMENT_NAME}/eval/"
+EVAL_OUTPUT_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/eval/"
 
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_search_paths="gins" \
-    --gin_file="hyper_base.gin" \
+    --gin_file="hyper_xl.gin" \
     --gin_file="instruction_embed.gin" \
     --gin_file="ni_eval.gin" \
     --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
@@ -18,7 +18,7 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin.USE_CACHED_TASKS=True \
     --gin.utils.DatasetConfig.batch_size=512 \
     --gin.utils.DatasetConfig.split=\"test\" \
-    --gin.partitioning.PjitPartitioner.num_partitions=2 \
+    --gin.partitioning.PjitPartitioner.num_partitions=8 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
