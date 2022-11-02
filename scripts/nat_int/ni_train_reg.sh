928da46c3bcb9d9232fea7a6955fa76ea5eb770d
Hamish Ivison <hamishivi@gmail.com>
2022-11-02 09:46:20 -0700

    baseline with positives

--- scripts/nat_int/ni_train_reg.sh
+++ scripts/nat_int/ni_train_reg.sh
@@ -18,14 +18,14 @@ python3 -m t5x.train \
   --gin.hyper_network.HyperT5Config.use_adapter=False \
   --gin.hyper_network.HyperT5Config.use_prefix=False \
   --gin.hyper_network.HyperT5Config.use_instructions=False \
-  --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
+  --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def_pos_2\" \
   --gin.USE_CACHED_TASKS=True \
   --gin.trainer.Trainer.num_microbatches=8 \
   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
   --gin.BATCH_SIZE=1024 \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=1101000 \
-  --gin.partitioning.PjitPartitioner.num_partitions=2 \
+  --gin.partitioning.PjitPartitioner.num_partitions=8 \
   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000/\" \
 
 echo "Training done. Now evaluating all checkpoints..."
@@ -41,11 +41,11 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin.hyper_network.HyperT5Config.use_adapter=False \
     --gin.hyper_network.HyperT5Config.use_prefix=False \
     --gin.hyper_network.HyperT5Config.use_instructions=False \
-    --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
+    --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def_pos_2\" \
     --gin.USE_CACHED_TASKS=True \
     --gin.utils.DatasetConfig.batch_size=512 \
     --gin.utils.DatasetConfig.split=\"test\" \
-    --gin.partitioning.PjitPartitioner.num_partitions=2 \
+    --gin.partitioning.PjitPartitioner.num_partitions=8 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
