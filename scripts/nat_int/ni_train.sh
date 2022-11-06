18c6f624b30852e1be963d03d69e97bcb1c42a1c
Hamish Ivison <hamishivi@gmail.com>
2022-11-06 11:06:06 -0800

    minor fix

--- scripts/nat_int/ni_train.sh
+++ scripts/nat_int/ni_train.sh
@@ -9,7 +9,7 @@ MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
 python3 -m t5x.train \
   --gin_search_paths=gins \
   --gin_file="hyper_xl.gin" \
-  --gin_file="instruction_embed.gin" \  # controls fid, adapters, prefixes
+  --gin_file="instruction_embed.gin" \
   --gin_file="ni_train.gin" \
   --gin_file="partial_train_adafactor.gin" \
   --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
