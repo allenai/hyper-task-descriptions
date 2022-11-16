026530ac6721bfbfe4d9bb9595178a3004746852
Hamish Ivison <hamishivi@gmail.com>
2022-11-15 16:44:02 -0800

    trying to use t5 xl

--- scripts/nat_int/ni_train_pretrained_froz.sh
+++ scripts/nat_int/ni_train_pretrained_froz.sh
@@ -29,7 +29,8 @@ python3 -m t5x.train \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=$4 \
   --gin.partitioning.PjitPartitioner.num_partitions=8 \
-  --gin.INITIAL_CHECKPOINT_PATH=\"gs://hamishi-us-bucket/$2/model/$3\"
+  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000/\"
+#\"gs://hamishi-us-bucket/$2/model/$3\"
 
 echo "Training done. Now evaluating all checkpoints..."
 
