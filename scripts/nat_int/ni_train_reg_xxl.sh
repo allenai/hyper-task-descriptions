335d72b349a0527246525bc61d67249631547ce4
Hamish Ivison <hamishivi@gmail.com>
2022-11-22 19:07:45 -0800

    stuff

--- scripts/nat_int/ni_train_reg_xxl.sh
+++ scripts/nat_int/ni_train_reg_xxl.sh
@@ -13,7 +13,6 @@ python3 -m t5x.train \
   --gin_search_paths=gins \
   --gin_file="hyper_xxl.gin" \
   --gin_file="ni_train.gin" \
-  --gin_file="partial_train_adafactor.gin" \
   --gin.hyper_network.HyperT5Config.hyperencoder_model=\"google/t5-base-lm-adapt\" \
   --gin.hyper_network.HyperT5Config.use_adapter=False \
   --gin.hyper_network.HyperT5Config.use_prefix=False \
@@ -25,9 +24,9 @@ python3 -m t5x.train \
   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
   --gin.BATCH_SIZE=1024 \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
-  --gin.TRAIN_STEPS=1001000 \
+  --gin.TRAIN_STEPS=1101000 \
   --gin.partitioning.PjitPartitioner.num_partitions=16 \
-  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_xxl/checkpoint_1000000/\" \
+  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xxl/checkpoint_1100000/\" \
 
 echo "Training done. Now evaluating all checkpoints..."
 # gsutil -m cp -r ${MODEL_DIR} gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model
