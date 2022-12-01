9713ae2b3edcbe7265ba4717625b418abadb1205
Hamish Ivison <hamishivi@gmail.com>
2022-11-30 17:21:39 -0800

    fix order

--- scripts/nat_int/ni_train_htune.sh
+++ scripts/nat_int/ni_train_htune.sh
@@ -32,8 +32,8 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_search_paths="gins" \
     --gin_file="hyper_xl.gin" \
     --gin_file="instruction_embed.gin" \
-    --gin_file="ni_eval.gin" \
     --gin_file="hypertune.gin" \
+    --gin_file="ni_eval.gin" \
     --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
     --gin.USE_CACHED_TASKS=True \
     --gin.utils.DatasetConfig.batch_size=512 \
