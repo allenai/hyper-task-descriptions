2765e013fe38fd27b3c476be43473add4c737bd4
Hamish Ivison <hamishivi@gmail.com>
2022-12-01 21:04:59 -0800

    trying some things

--- scripts/nat_int/ni_eval.sh
+++ scripts/nat_int/ni_eval.sh
@@ -10,14 +10,14 @@ EVAL_OUTPUT_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/eval/"
 
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_search_paths="gins" \
-    --gin_file="hyper_xxl.gin" \
+    --gin_file="hyper_xl.gin" \
     --gin_file="instruction_embed.gin" \
     --gin_file="ni_eval.gin" \
     --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
     --gin.USE_CACHED_TASKS=True \
-    --gin.utils.DatasetConfig.batch_size=32 \
+    --gin.utils.DatasetConfig.batch_size=512 \
     --gin.utils.DatasetConfig.split=\"test\" \
-    --gin.partitioning.PjitPartitioner.num_partitions=16 \
+    --gin.partitioning.PjitPartitioner.num_partitions=8 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
