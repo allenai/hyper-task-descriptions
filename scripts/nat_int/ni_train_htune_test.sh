9c864ed0f9b6fbe914326ad6056c56de91664fb6
Hamish Ivison <hamishivi@gmail.com>
2022-12-03 22:35:01 -0800

    try reduce size?

--- scripts/nat_int/ni_train_htune_test.sh
+++ scripts/nat_int/ni_train_htune_test.sh
@@ -28,7 +28,7 @@ python3 -m t5x.train \
   --gin.BATCH_SIZE=1024 \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=$4 \
-  --gin.partitioning.PjitPartitioner.num_partitions=8 \
+  --gin.partitioning.PjitPartitioner.num_partitions=16 \
   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000/\"
 
 
