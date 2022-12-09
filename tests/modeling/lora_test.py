36a0f2be2d9529119f81ed42652ac678764958cd
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-12-08 23:28:33 -0800

    My LoRA changes (#66)
    LoRA changes

--- tests/modeling/lora_test.py
+++ tests/modeling/lora_test.py
@@ -7,9 +7,11 @@ from hyper_task_descriptions.common.testing import get_prng_key
 from hyper_task_descriptions.modeling.lora import (
     LoraDenseGeneral,
     LoraMultiHeadDotProductAttentionWithPrefix,
+    batch_ia3_linear,
+    batch_lora_ia3_linear,
     batch_lora_linear,
-    efficient_batch_lora_linear,
-    efficient_lora_linear,
+    ia3_linear,
+    lora_ia3_linear,
     lora_linear,
 )
 
@@ -27,11 +29,81 @@ def test_lora_linear():
     output = lora_linear(inputs, W, A, B, 1, rank)
     assert output.shape == (batch_size, out_features)
     expected_output = (inputs @ W) + (inputs @ A @ B) * (1 / rank)  # W0x + (BAx*scaling)
-    assert jnp.all(output == expected_output)
 
-    efficient_output = efficient_lora_linear(inputs, W, A, B, 1, rank)
-    assert efficient_output.shape == (batch_size, out_features)
-    np.testing.assert_allclose(efficient_output, expected_output, rtol=1e-6)
+    np.testing.assert_allclose(output, expected_output, rtol=1e-6)
+
+
+def test_ia3_linear():
+    batch_size, in_features, out_features = 3, 4, 5
+    rank = 2
+
+    key = get_prng_key(23)
+    inputs = jax.random.normal(key, (batch_size, in_features))
+    W = jax.random.normal(key, (in_features, out_features))
+    A = jax.random.normal(key, (in_features, rank))
+    B = jax.random.normal(key, (rank, out_features))
+
+    output = ia3_linear(inputs, W, A, B, 1, rank)
+    assert output.shape == (batch_size, out_features)
+    expected_output = inputs @ (W * (A @ B) * (1 / rank))
+
+    np.testing.assert_allclose(output, expected_output, rtol=1e-6)
+
+
+def test_lora_ia3_linear():
+    batch_size, in_features, out_features = 3, 4, 5
+    rank = 2
+    ia3_rank = 4
+
+    key = get_prng_key(23)
+    inputs = jax.random.normal(key, (batch_size, in_features))
+    W = jax.random.normal(key, (in_features, out_features))
+    A = jax.random.normal(key, (in_features, rank))
+    B = jax.random.normal(key, (rank, out_features))
+
+    ia3_A = jax.random.normal(key, (in_features, ia3_rank))
+    ia3_B = jax.random.normal(key, (ia3_rank, out_features))
+
+    output = lora_ia3_linear(inputs, W, A, B, ia3_A, ia3_B, 1, rank, ia3_rank)
+    assert output.shape == (batch_size, out_features)
+
+    ia3_kernel = W * (ia3_A @ ia3_B) * (1 / ia3_rank)
+    lora_kernel = ia3_kernel + ((A @ B) * (1 / rank))
+    expected_output = inputs @ lora_kernel
+
+    np.testing.assert_allclose(output, expected_output, rtol=1e-6)
+
+
+def test_batch_lora_ia3_linear():
+    batch_size, in_features, out_features = 3, 4, 5
+    rank = 2
+    ia3_rank = 4
+
+    key = get_prng_key(23)
+    inputs = jax.random.normal(key, (batch_size, in_features))
+    W = jax.random.normal(key, (in_features, out_features))
+    A = jax.random.normal(key, (batch_size, in_features, rank))
+    B = jax.random.normal(key, (batch_size, rank, out_features))
+
+    ia3_A = jax.random.normal(key, (batch_size, in_features, ia3_rank))
+    ia3_B = jax.random.normal(key, (batch_size, ia3_rank, out_features))
+
+    output = batch_lora_ia3_linear(inputs, W, A, B, ia3_A, ia3_B, 1, rank, ia3_rank)
+    assert output.shape == (batch_size, out_features)
+
+
+def test_batch_ia3_linear():
+    batch_size, in_features, out_features = 3, 4, 5
+    rank = 2
+
+    key = get_prng_key(23)
+    inputs = jax.random.normal(key, (batch_size, in_features))
+    W = jax.random.normal(key, (in_features, out_features))
+    A = jax.random.normal(key, (batch_size, in_features, rank))
+    B = jax.random.normal(key, (batch_size, rank, out_features))
+
+    output = batch_ia3_linear(inputs, W, A, B, 1, rank)
+    assert output.shape == (batch_size, out_features)
 
 
 def test_batch_lora_linear():
@@ -46,10 +118,6 @@ def test_batch_lora_linear():
 
     output = batch_lora_linear(inputs, W, A, B, 1, rank)
     assert output.shape == (batch_size, out_features)
-
-    efficient_output = efficient_batch_lora_linear(inputs, W, A, B, 1, rank)
-    assert output.shape == (batch_size, out_features)
-    np.testing.assert_allclose(output, efficient_output, rtol=1e-5)
 
 
 def test_lora_dense_general():
