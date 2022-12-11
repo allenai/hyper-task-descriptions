70bb3a6feecf8895818f5f72dd93a8060e3c16e0
Hamish Ivison <hamishivi@gmail.com>
2022-12-11 21:55:01 +0400

    adapter size stuff

--- scripts/pretrain_decoder.sh
+++ scripts/pretrain_decoder.sh
@@ -11,7 +11,7 @@ python3 -m t5x.train \
   --gin_file="partial_train_adafactor.gin" \
   --gin_file="hypertune.gin" \
   --gin.hyper_network.HyperT5Config.use_adapter=True \
-  --gin.hyper_network.HyperT5Config.use_adapter.adapter_size=64 \
+  --gin.hyper_network.HyperT5Config.adapter_size=64 \
   --gin.USE_CACHED_TASKS=True \
   --gin.trainer.Trainer.num_microbatches=32 \
   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
