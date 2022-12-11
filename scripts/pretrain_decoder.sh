a7b762dba1c7aaa1dd52d6013ce2c59d32cbaf2d
Hamish Ivison <hamishivi@gmail.com>
2022-12-11 22:39:04 +0400

    stuff

--- scripts/pretrain_decoder.sh
+++ scripts/pretrain_decoder.sh
@@ -13,7 +13,7 @@ python3 -m t5x.train \
   --gin.hyper_network.HyperT5Config.use_adapter=True \
   --gin.hyper_network.HyperT5Config.adapter_size=64 \
   --gin.USE_CACHED_TASKS=True \
-  --gin.trainer.Trainer.num_microbatches=32 \
+  --gin.trainer.Trainer.num_microbatches=64 \
   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
   --gin.BATCH_SIZE=1024 \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
