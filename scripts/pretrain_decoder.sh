d169e1cc288231e465272441be046f69351d005f
Hamish Ivison <hamishivi@gmail.com>
2022-12-13 11:49:13 +1100

    Decoder too expensive

--- scripts/pretrain_decoder.sh
+++ scripts/pretrain_decoder.sh
@@ -8,13 +8,13 @@ python3 -m t5x.train \
   --gin_file="hyper_xl.gin" \
   --gin_file="instruction_embed.gin" \
   --gin_file="pretrain.gin" \
-  --gin_file="partial_train_adafactor.gin" \
-  --gin_file="hypertune_full_train.gin" \
+  --gin_file="train_only_hnet.gin" \
+  --gin_file="hypertune.gin" \
   --gin.USE_CACHED_TASKS=True \
-  --gin.trainer.Trainer.num_microbatches=64 \
+  --gin.trainer.Trainer.num_microbatches=8 \
   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
   --gin.BATCH_SIZE=1024 \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=1120000 \
-  --gin.partitioning.PjitPartitioner.num_partitions=16 \
+  --gin.partitioning.PjitPartitioner.num_partitions=8 \
   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000/\"
