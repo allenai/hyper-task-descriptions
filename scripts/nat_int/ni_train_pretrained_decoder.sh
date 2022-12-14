3859c17f6dab9632c107d35f23e2720fb910245c
Hamish Ivison <hamishivi@gmail.com>
2022-12-14 20:18:36 +1100

    fix script

--- scripts/nat_int/ni_train_pretrained_decoder.sh
+++ scripts/nat_int/ni_train_pretrained_decoder.sh
@@ -21,7 +21,7 @@ python3 -m t5x.train \
   --gin_file="ni_train.gin" \
   --gin_file="hypertune.gin" \
   --gin_file="full_restore.gin" \
-  --gin_file="train_only_hnet.gin" 
+  --gin_file="train_only_hnet.gin" \
   --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
   --gin.USE_CACHED_TASKS=True \
   --gin.trainer.Trainer.num_microbatches=64 \
