cb7d82659919ec4034da93512a2d31faedf840d8
Hamish Ivison <hamishivi@gmail.com>
2022-12-17 22:52:19 +1100

    pretrain base

--- scripts/pretrain_base.sh
+++ scripts/pretrain_base.sh
@@ -15,5 +15,5 @@ python3 -m t5x.train \
   --gin.BATCH_SIZE=1024 \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=2000000 \
-  --gin.partitioning.PjitPartitioner.num_partitions=8 \
+  --gin.partitioning.PjitPartitioner.num_partitions=2 \
   --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000/\"
