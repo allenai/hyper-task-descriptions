75a9dba864d19e583f066ce55700a3d662eaf5ef
Hamish Ivison <hamishivi@gmail.com>
2023-01-06 14:44:08 -0800

    no hnet baseline

--- scripts/nat_int/ni_train_no_hnet.sh
+++ scripts/nat_int/ni_train_no_hnet.sh
@@ -13,7 +13,7 @@ python3 -m t5x.train \
   --gin_file="ni_train.gin" \
   --gin_file="partial_train_adafactor_dual.gin" \
   --gin.hyper_network.HyperT5Config.use_instructions=False \
-  --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
+  --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
   --gin.USE_CACHED_TASKS=True \
   --gin.trainer.Trainer.num_microbatches=16 \
   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
@@ -34,7 +34,7 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_file="instruction_embed.gin" \
     --gin_file="ni_eval.gin" \
     --gin.hyper_network.HyperT5Config.use_instructions=False \
-    --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions\" \
+    --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
     --gin.USE_CACHED_TASKS=True \
     --gin.utils.DatasetConfig.batch_size=512 \
     --gin.utils.DatasetConfig.split=\"test\" \
