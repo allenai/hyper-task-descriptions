321b78b546ec4c01e4b862973b7b83314c79cb5f
Hamish Ivison <hamishivi@gmail.com>
2023-01-06 22:56:02 -0800

    adapter xxl

--- scripts/nat_int/ni_train_no_hnet.sh
+++ scripts/nat_int/ni_train_no_hnet.sh
@@ -8,7 +8,7 @@ MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
 
 python3 -m t5x.train \
   --gin_search_paths=gins \
-  --gin_file="hyper_base.gin" \
+  --gin_file="hyper_xxl.gin" \
   --gin_file="instruction_embed.gin" \
   --gin_file="ni_train.gin" \
   --gin_file="partial_train_adafactor_dual.gin" \
@@ -16,13 +16,13 @@ python3 -m t5x.train \
   --gin.hyper_network.HyperT5Config.use_fusion_in_decoder=False \
   --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
   --gin.USE_CACHED_TASKS=True \
-  --gin.trainer.Trainer.num_microbatches=16 \
+  --gin.trainer.Trainer.num_microbatches=32 \
   --gin.utils.create_learning_rate_scheduler.warmup_steps=100 \
   --gin.BATCH_SIZE=1024 \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.TRAIN_STEPS=1101000 \
-  --gin.partitioning.PjitPartitioner.num_partitions=2 \
-  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_base/checkpoint_1100000/\"
+  --gin.partitioning.PjitPartitioner.num_partitions=16 \
+  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xxl/checkpoint_1100000/\"
 
 echo "Training done. Now evaluating all checkpoints..."
 
@@ -31,7 +31,7 @@ EVAL_OUTPUT_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/eval/"
 
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin_search_paths="gins" \
-    --gin_file="hyper_base.gin" \
+    --gin_file="hyper_xxl.gin" \
     --gin_file="instruction_embed.gin" \
     --gin_file="ni_eval.gin" \
     --gin.hyper_network.HyperT5Config.use_instructions=False \
@@ -40,7 +40,7 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin.USE_CACHED_TASKS=True \
     --gin.utils.DatasetConfig.batch_size=512 \
     --gin.utils.DatasetConfig.split=\"test\" \
-    --gin.partitioning.PjitPartitioner.num_partitions=2 \
+    --gin.partitioning.PjitPartitioner.num_partitions=16 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
     --gin.utils.RestoreCheckpointConfig.mode=\"all\" \
     --gin.EVAL_OUTPUT_DIR=\"$EVAL_OUTPUT_DIR\"
