4b2b790b246307b8a6b3207ad8935d315ec8495e
Hamish Ivison <hamishivi@gmail.com>
2023-01-07 13:07:42 -0800

    xxl too big

--- scripts/nat_int/ni_train_no_hnet.sh
+++ scripts/nat_int/ni_train_no_hnet.sh
@@ -38,7 +38,7 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.eval \
     --gin.hyper_network.HyperT5Config.use_fusion_in_decoder=False \
     --gin.MIXTURE_OR_TASK_NAME=\"natural_instructions_def\" \
     --gin.USE_CACHED_TASKS=True \
-    --gin.utils.DatasetConfig.batch_size=512 \
+    --gin.utils.DatasetConfig.batch_size=128 \
     --gin.utils.DatasetConfig.split=\"test\" \
     --gin.partitioning.PjitPartitioner.num_partitions=16 \
     --gin.CHECKPOINT_PATH=\"$MODEL_DIR\" \
