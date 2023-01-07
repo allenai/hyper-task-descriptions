c69446f8daf3847493fe270276acaf8ace19d349
Hamish Ivison <hamishivi@gmail.com>
2023-01-07 15:06:31 -0800

    hypter p3

---
+++ scripts/t0_train_hypter.sh
@@ -0,0 +1,20 @@
+# name of experiment folder
+EXPERIMENT_NAME=$1
+
+# where model will be saved
+MODEL_DIR="gs://hamishi-us-bucket/${EXPERIMENT_NAME}/model"
+
+# we go offline to avoid constant calls to get basic info (happens even when cached)
+# for your first run, you will probably need to run all these calls :(
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 python3 -m t5x.train \
+  --gin_search_paths=gins \
+  --gin_file="hyper_xl.gin" \
+  --gin_file="instruction_embed.gin" \
+  --gin_file="t0_train.gin" \
+  --gin_file="partial_train_adafactor_no_roberta.gin" \
+  --gin_file="hypter.gin" \
+  --gin.MODEL_DIR=\"${MODEL_DIR}\" \
+  --gin.TRAIN_STEPS=1120000 \
+  --gin.partitioning.PjitPartitioner.num_partitions=8 \
+  --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000\" \
+  --tfds_data_dir="gs://hamishi-us-bucket/t0_data/data"
