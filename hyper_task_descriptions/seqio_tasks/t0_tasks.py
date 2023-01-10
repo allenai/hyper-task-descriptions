612f14da23747ca7b32199cd8a0fb0266b767381
Hamish Ivison <hamishivi@gmail.com>
2023-01-10 09:36:55 -0800

    hypter p3 we want split

--- hyper_task_descriptions/seqio_tasks/t0_tasks.py
+++ hyper_task_descriptions/seqio_tasks/t0_tasks.py
@@ -22,7 +22,7 @@ from hyper_task_descriptions.hf_vocab import HuggingfaceVocabulary
 from hyper_task_descriptions.seqio_tasks import utils
 
 # cached locations for everything - required to find data.
-seqio.add_global_cache_dirs(["gs://hamishi-us-bucket/t0_data_edited_prompts"])
+seqio.add_global_cache_dirs(["gs://hamishi-us-bucket/t0_data_split_all_t5"])
 
 GET_METRICS = {
     "BLEU": mt.bleu,
